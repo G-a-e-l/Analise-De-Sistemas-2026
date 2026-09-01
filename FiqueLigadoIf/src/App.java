09c4c525c5f2d00429c02b81557eb5e8b70c3c4d
G-a-e-l <gael.miranda@estudante.iftm.edu.br>
2026-09-01 10:42:04 -0300

    Enhance App with Local, Pessoa, and Usuario instances
    Updated the main method to include new instances of Local, Pessoa, and Usuario classes with relevant details.

--- FiqueLigadoIf/src/App.java
+++ FiqueLigadoIf/src/App.java
@@ -1,9 +1,34 @@
 import domain.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Area area = new Area (1,"Desenvolvimento de sistemas");
+
+        Area area = new Area(1, "Computação");
+
+        Local local = new Local(1,
+                "Auditorio IFTM",
+                "Rua Blanche",
+                2033,
+                "Morada da Colina",
+                "Uberlândia",
+                "123456789",
+                10,
+                "No campus");
+
+        Pessoa pessoa = new Pessoa(1,
+                "Carlos Eduardo",
+                "/home/cadu.jpg",
+                "Profe de analises",
+                "cadu@iftm.edu.br");
+
+        Usuario usuario = new Usuario(1,
+                "senha",
+                TipoUsuarioEnum.SERVIDOR,
+                pessoa);
+
         System.out.println(area);
-        
+        System.out.println(local);
+        System.out.println(pessoa);
+        System.out.println(usuario);
     }
 }
