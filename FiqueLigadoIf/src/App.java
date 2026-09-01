3c1083ec128263ad8695883fbc5d08ad43fc0ba0
G-a-e-l <gael.miranda@estudante.iftm.edu.br>
2026-09-01 10:12:51 -0300

    Add files via upload

---
+++ FiqueLigadoIf/src/App.java
@@ -0,0 +1,9 @@
+import domain.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Area area = new Area (1,"Desenvolvimento de sistemas");
+        System.out.println(area);
+        
+    }
+}
