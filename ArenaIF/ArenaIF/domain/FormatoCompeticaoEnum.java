ba4d7e303a726074f44397fe1c11e5eaa71b4cf1
G-a-e-l <gael.miranda@estudante.iftm.edu.br>
2026-09-12 21:49:05 -0300

    Delete ArenaIF/ArenaIF directory

--- ArenaIF/ArenaIF/domain/FormatoCompeticaoEnum.java
+++
@@ -1,8 +0,0 @@
-package domain;
-
-public enum FormatoCompeticaoEnum {
-
-    ELIMINATORIA_SIMPLES,
-    PONTOS_CORRIDOS,
-    FASE_DE_GRUPOS;
-}
--- ArenaIF/ArenaIF/domain/SituacaoPartidaEnum.java
+++
@@ -1,9 +0,0 @@
-package domain;
-
-public enum SituacaoPartidaEnum {
-
-    AGENDADA,
-    EM_ANDAMENTO,
-    FINALIZADA,
-    CANCELADA;
-}
